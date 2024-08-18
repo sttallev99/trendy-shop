d78c83ebbd3d637a5d33fd892d7886425c179006
Георги Сталев <sttallev99@gmail.com>
2024-08-18 13:04:00 +0300

    Creating pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/"  element={<Home />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
