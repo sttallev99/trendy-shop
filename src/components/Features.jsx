80a7234cf14211aa5d28b0000f240e59baae0b91
Георги Сталев <sttallev99@gmail.com>
2024-08-19 01:35:50 +0300

    Customize Features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import {LiaShippingFastSolid} from 'react-icons/lia'
+import {MdCurrencyExchange} from 'react-icons/md'
+import {BiSupport} from 'react-icons/bi'
+import {TbPackageImport} from 'react-icons/tb'
 
 const Features = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='max-padd-container bg-primary mt-16 xl:mt-18 py-8 rounded-xl'>
+      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8'>
+        <div className='flexCenter gap-x-4'>
+          <LiaShippingFastSolid className="text-4xl"/>
+          <div>
+            <h5 className='medium-18'>Free Shipping</h5>
+            <p>On above $100 order</p>
+          </div>
+        </div>
+        <div className='flexCenter gap-x-4'>
+          <MdCurrencyExchange className="text-4xl"/>
+          <div>
+            <h5 className='medium-18'>Free Shipping</h5>
+            <p>On above $100 order</p>
+          </div>
+        </div>
+        <div className='flexCenter gap-x-4'>
+          <BiSupport className="text-4xl"/>
+          <div>
+            <h5 className='medium-18'>Free Shipping</h5>
+            <p>On above $100 order</p>
+          </div>
+        </div>
+        <div className='flexCenter gap-x-4'>
+          <TbPackageImport className="text-4xl"/>
+          <div>
+            <h5 className='medium-18'>Free Shipping</h5>
+            <p>On above $100 order</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
