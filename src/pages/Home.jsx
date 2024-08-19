f14166e0f994b627b8084512ced0d087181d841e
Георги Сталев <sttallev99@gmail.com>
2024-08-19 12:34:48 +0300

    Customize category section and create ProductDisplay component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
 import Categories from '../components/Categories'
+import ProductDisplay from '../components/ProductDisplay'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Features />
       <Categories />
+      <ProductDisplay />
     </>
   )
 }
