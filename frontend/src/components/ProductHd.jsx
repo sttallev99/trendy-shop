745b946c61b848e09334e547e756514f83713553
Георги Сталев <sttallev99@gmail.com>
2024-08-23 01:33:09 +0300

    Revert "Make changes"
    This reverts commit 36e1d214db60f6d2918491781da229b589a782a6.

--- frontend/src/components/ProductHd.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {TbArrowRight} from 'react-icons/tb'
-
-export const ProductHd = ({product}) => {
-  return (
-    <div className='max-padd-container flex items-center flex-wrap gap-x-2 medium-16 py-4 capitalize bg-primary'>Home <TbArrowRight /> {product.name}</div>
-  )
-}
