46ce7e1c8b742d255270809fbee030d58fe8f22e
Георги Сталев <sttallev99@gmail.com>
2024-08-26 02:47:19 +0300

    Implement verify payment functionality

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {ShopContext} from "../context/ShopContext";
+import axios from 'axios';
 
 const Verify = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+
+  const { url } = useContext(ShopContext);
+  const navigate = useNavigate();
+
+  const verifyPayment = async() => {
+    const response = await axios.post(url+"/api/order/verify", {success, orderId});
+    if(response.data.success) {
+      navigate("/myorders")
+    } else {
+      navigate("/")
+    }
+  }
+
+  useEffect(() => {
+    verifyPayment()
+  }, [])
+
   return (
-    <div>
-      Verify
-    </div>
+    <section>
+      <div className='min-h-[60vh] grid'>
+        <div className='h-24 w-24 place-self-center border-4 border-t-secondary rounded-full animate-spin'></div>
+      </div>
+    </section>
   )
 }
 
