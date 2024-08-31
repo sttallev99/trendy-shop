83c827670190505ae80e59ba7cb84ef786d4f333
Георги Сталев <sttallev99@gmail.com>
2024-08-31 11:42:27 +0300

    made changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
   return (
     <section id='home' className='max-padd-container bg-hero bg-center bg-cover bg-no-repeat h-[777px] w-full max-[640px]:bg-left backdrop-blur-md'>
       <div className='relative max-w-[666px] top-44 xs:top-72'>
+        <p className='pt-2 pb-10'>If you load the app for the first time it may take a few seconds longer. Thanks for you understanding.</p>
         <h4 className='flex items-baseline gap-x-2 uppercase text-secondary medium-18'>Modern collection <BsFire /></h4>
         <h2 className='h1 capitalize'>Grab Upto 50% Off on Selected Products</h2>
         <p className='border-l-4 border-secondary pl-3 my-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
